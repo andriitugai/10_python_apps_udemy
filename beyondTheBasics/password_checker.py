cb2a5dbee48c2f859d20fe81049c88575a22fe84
Andrii Tugai <andrii.tugai@gmail.com>
2019-01-15 03:23:04 +0200

    the first commit

---
+++ beyondTheBasics/password_checker.py
@@ -0,0 +1,7 @@
+correct_password = 'python123'
+password = input("Input the password: ")
+
+while( password != correct_password):
+    password = input("Wrong password! Input again: ")
+
+print("Yes!")
---
+++ beyondTheBasics/while_loop.py
@@ -0,0 +1,6 @@
+x = 0
+while x < 3:
+    print(str(x) + ": Less than 3")
+    x = x + 1
+
+    
